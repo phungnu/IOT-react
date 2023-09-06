1cdcbb03f1ef132618d14f99b9a33bd96f8e5341
unknown <phungnu15032002@gmail.com>
2023-09-06 08:14:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Line } from "react-chartjs-2";
+import { Row, Col,Switch, Image } from 'antd';
+import Chart from 'chart.js/auto';
+import {CategoryScale} from 'chart.js'; 
+Chart.register(CategoryScale);
+
+const data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: false,
+      borderColor: "rgb(229, 229, 73)"
+    },
+    {
+      label: "Second dataset",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: false,
+      borderColor: "rgba(255, 102, 0, 0.656)"
+    },
+    {
+      label: "Third dataset",
+      data: [35, 27, 30, 51, 54, 76],
+      fill: false,
+      borderColor: "rgb(40, 199, 40)"
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col span={8} className='row1 box' >
+          <div className='temp all'>
+            <h3>Nhiệt độ</h3>
+            <h2>20</h2>
+          </div>
+        </Col>
+        <Col span={8} className='row1 box' >
+          <div className='humidity all'>
+            <h3>Độ ẩm</h3>
+            <h2>80%</h2>
+          </div>
+        </Col>
+        <Col span={8} className='row1 box'>
+          <div className='light all'>
+            <h3>Ánh sáng</h3>
+            <h2>100 lux</h2>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={16} className='row2'>
+          <Line data={data} />
+         </Col>
+        <Col span={8} className='row2' >
+          <Row className='row1 box top'>
+            <Col span={24} className='all switch'>
+              <Image className='bulb' src={'/2779262.png'} width={60} preview={false}/>
+              <Switch size="default" defaultChecked />
+            </Col>
+          </Row>
+          <Row className='row1 box top'>
+            <Col span={24} className='all switch'>
+              <Image className='bulb' src={'/fan.png'} width={60} preview={false}/>
+              <Switch size="default" defaultChecked />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 }
